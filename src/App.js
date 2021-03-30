f03ee1b50d96879367e0f53a35318b4839c59c3d
Stephen Lubuulwa <stephenlubulwa@gmail.com>
2021-03-30 10:09:40 +0300

    Pretty much done the exercise, skeptical about The credit, might have misunderstood it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// useState: greeting
+
+import React, { useState } from 'react';
+
+function Greeting() {
+	// 💣 delete this variable declaration and replace it with a React.useState call
+	const [name, setName] = useState('Stephen');
+
+	function handleChange(event) {
+		// 🐨 update the name here based on event.target.value
+		setName(event.target.value);
+	}
+
+	return (
+		<div>
+			<form>
+				<label htmlFor='name'>Name: </label>
+				<input onChange={handleChange} id='name' />
+			</form>
+			{name ? <strong>Welcome, {name}</strong> : 'Please type your name'}
+		</div>
+	);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <Greeting />;
 }
 
 export default App;
